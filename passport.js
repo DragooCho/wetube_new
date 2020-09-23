c5d863399def37c6302ff86feeee2e6aa58b0777
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-09-23 16:06:20 +0900

    6.1 end

--- passport.js
+++ passport.js
@@ -1,4 +1,6 @@
 import passport from "passport";
 import User from "./models/User";
 
-passport.use(User.creeteStrate());
+passport.use(User.creeteStrategy());
+
+//strategy는 로그인 하는 방식...즉 깃헙,페이스북 같은username과 password로 로그인 하는것들을 해줌
