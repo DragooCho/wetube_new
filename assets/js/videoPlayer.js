fed3a19b9f3f9d1521e7e150130eb10287214aee
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-10-03 22:05:20 +0900

    wetube 8.1 done

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,3 +1,22 @@
-const videoContainer = document.getElementById("jsvVideoPlayer");
+const videoContainer = document.getElementById("jsVideoPlayer");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
+const playBtn = document.getElementById("jsPlayButton");
 
-videoContainer.addEventListener("click", () => false);
+function handlePlayClick() {
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+  } else {
+    videoPlayer.pause();
+  }
+}
+
+function init() {
+  playBtn.addEventListener("click", handlePlayClick);
+}
+
+if (videoContainer) {
+  init();
+}
+
+// 예로 들어 여기서 HTMLMediaElement.play() 를 활용할려면
+// videoPlayer.play()를 써야함
