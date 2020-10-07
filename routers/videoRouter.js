3e6562a958841af83d10ce736f914c8e98b0a0f9
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-10-07 13:15:06 +0900

    wetube #9 end

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -8,7 +8,7 @@ import {
   getEditVideo,
   postEditVideo,
 } from "../controllers/videoController";
-import { onlyPrivate, uploadVideo } from "../middlewares";
+import { uploadVideo, onlyPrivate } from "../middlewares";
 
 const videoRouter = express.Router();
 
